e39dfc992c9d272ee498def32a8bb56d948af6a0
micnori <micnori@users.noreply.github.com>
2024-05-20 13:38:46 +0200

    fix api url in test

--- test_oauth.py
+++ test_oauth.py
@@ -7,11 +7,11 @@ service_information = ServiceInformation(
     token_service='https://aac.digitalhub-dev.smartcommunitylab.it/oauth/token',
     client_id='',
     client_secret='',
-    scopes=['tenantcoreclient']
+    scopes=['omd']
 )
 
 manager = CredentialManager(service_information)
 manager.init_with_client_credentials()
 print(f"access token:{manager._access_token}")
-for itemNode in CoreHelper.getDataItems("https://core.tenant1.digitalhub-dev.smartcommunitylab.it/api/v1/dataitems", manager._access_token):
+for itemNode in CoreHelper.getDataItems("https://core.tenant1.digitalhub-dev.smartcommunitylab.it/api/v1", manager._access_token):
     print(json.dumps(itemNode))
